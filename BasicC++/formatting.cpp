488c01e46669e482453a6332ae164c8bb04e4847
t-yeasmin <tawfiafeqir@gmail.com>
2022-06-17 13:43:09 +0600

    adding formatting.cpp

---
+++ BasicC++/formatting.cpp
@@ -0,0 +1,9 @@
+// u always want to show urself as a queen? formatting is kind of makeup for program
+//syntax is : cout << "anything :"" << grade;
+#include <iostream>
+using namespace std;
+int main(){
+char grade = 'A';
+cout << "Grade is : "<<grade;
+return 0;
+}
